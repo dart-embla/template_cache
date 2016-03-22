8d58ba3ae2366b449e319eb8d4db76e3d91c2f4f
Emil Persson <emil.persson@creuna.se>
2016-03-22 21:58:27 +0100

    Provide error message in helper

--- lib/src/render.dart
+++ lib/src/render.dart
@@ -10,5 +10,8 @@ Stream<String> render(String file, {Map<Symbol, dynamic> locals: const {}}) {
 
 Template view(String file, {Map<Symbol, dynamic> locals: const {}}) {
   final id = uid(new Uri.file(file));
+  if (_gen.templates.containsKey(id)) {
+    throw new Exception('$file is not compiled! Try running "pub run template_cache:compile $file"');
+  }
   return _gen.templates[id](locals);
 }
