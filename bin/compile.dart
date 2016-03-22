0b951cd6191baec159b0424a820e98b496fe34c5
Emil Persson <emil.persson@creuna.se>
2016-03-22 20:49:42 +0100

    Explicitly get working directory in pub commands

--- bin/compile.dart
+++ bin/compile.dart
@@ -1,9 +1,11 @@
 import 'package:template_cache/cache.dart';
+import 'package:path/path.dart' as path;
+import 'dart:io';
 
 main(List<String> args) {
   if (args.length != 1) {
     return print('Usage: pub run template_cache:compile my_template_file.html');
   }
   final cache = new Cache();
-  cache.compile(args[0]);
+  cache.compile(path.join(Directory.current.path, args[0]));
 }
--- bin/render.dart
+++ bin/render.dart
@@ -1,8 +1,10 @@
 import 'package:template_cache/render.dart';
+import 'package:path/path.dart' as path;
+import 'dart:io';
 
 main(List<String> args) {
   if (args.length != 1) {
     return print('Usage: pub run template_cache:render my_template_file.html');
   }
-  render(args[0]).join().then(print);
+  render(path.join(Directory.current.path, args[0]));
 }
